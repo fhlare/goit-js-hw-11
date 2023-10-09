dc8017e46a0619a7fd35b6ca3dd599d3a481b579
fhlare <127776539+fhlare@users.noreply.github.com>
2023-10-09 09:29:05 +0300

    Update fetch.js

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -3,8 +3,9 @@ import axios from 'axios';
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '38965444-221e39e59f698a8ee4d2c4c8b';
 
-export async function getImage(searchQuery, page, perPage, ) {
-  const params = new URLSearchParams({
+export async function getImage(searchQuery, page, perPage,) {
+  try {
+      const params = new URLSearchParams({
     q: searchQuery,
     image_type: 'photo',
     orientation: 'horizontal',
@@ -15,4 +16,8 @@ export async function getImage(searchQuery, page, perPage, ) {
 
   const response = await axios.get(`${BASE_URL}?key=${API_KEY}&${params}`);
   return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+
 }
